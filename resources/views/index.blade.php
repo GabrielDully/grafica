ae6520b36adf40e0e93eaf6efdbfe9b86bf77783
gabrieldully <gabrieldully@usp.br>
2024-12-13 11:45:27 -0300

    Desenvolvimento front-end da homepage para usuário guest  - index.blade.php: inserção dos botões de login para Editora e Gráfica;  - index.blade.php: redisposição do conteúdo conforme o layout e design sugerido pelo Sistema de Estágios.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,47 @@
 @section('content')
 
 @if(Auth::guest())
-  Sistema para solicitação de serviços da Editora e Gráfica
+  <div class="card">
 
-  <br><br>
+    <div class="card-header"><b>Sistema para solicitação de serviços da Editora e Gráfica</b></a></div>
+    
+    <div class="card-body">
+      <div class="row">
+        <div class="col-sm">
+          <a href="{{ $app_url }}/login/usp" class="btn btn-success"> 
+            <i class="fa fa-university" aria-hidden="true"></i>
+            Login USP 
+          </a>
+        </div>
 
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/8VSvUe_r4dA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-@else
+        <div class="col-sm">
+          <form method="GET" action="{{ $app_url }}/login/empresa">
+            @csrf
+            
+            <button type="submit" class="btn btn-success" name="login_action" value="email">
+              <i class="fa fa-building" aria-hidden="true"></i>  Editora / Gráfica - Primeiro Acesso
+            </button>
+
+            <button type="submit" class="btn btn-success" name="login_action" value="senha">
+              <i class="fa fa-building" aria-hidden="true"></i>  Login com Senha
+            </button>
+          </form>
+
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <div class="col-sm">
+
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/8VSvUe_r4dA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  @else
     @include('dashboard')
 @endif
 
